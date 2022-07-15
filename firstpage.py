f9fbe070bcaf701b9be2e45170e9280b6068809e
DaeeeD <viterbogregoire@gmail.com>
2022-07-15 16:35:28 +0200

    first page

--- firstpage.py
+++ firstpage.py
@@ -10,17 +10,26 @@ class InfinitePlayer(QtWidgets.QWidget):
         super().__init__()
         self.WelcomeToInfinite = ["hello there! welcome to the infinite player !"]
         self.button = QtWidgets.QPushButton("Just a placeholder button ! ")
+        self.button2 = QtWidgets.QComboBox()
         self.text = QtWidgets.QLabel("Infinity Player", alignment=QtCore.Qt.AlignCenter)
+    
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
+        self.layout.addWidget(self.button2)
         self.button.clicked.connect(fetchingthealbum)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
     widget = InfinitePlayer()
+    widget.setStyleSheet("""
+        background-color: #262626;
+        color: #FFFFFF;
+        font-family: Titillium;
+        font-size: 18px;
+        """)
     widget.resize(800, 600)
     widget.show()
 
